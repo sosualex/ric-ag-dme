9d8100b2fb1d5194853bc222e47dd205311b572c
Sosu Alex <sosualex@gmail.com>
2021-10-20 23:58:51 +0530

    class version of node

--- app/dcs-init.js
+++ app/dcs-init.js
@@ -1,3 +1,28 @@
 //code to initialize the distributed computing system
+const log = require('../log');
+const Dnode = require('./dme-node');
+
+let nodes = []
+let node1 = new Dnode(1, 3)
+let node2 = new Dnode(2, 3)
+let node3 = new Dnode(3, 3)
+log(node1)
+log(node2)
+log(node3)
+node1.advanceClock(0)
+node1.advanceClock(5)
+node1.advanceClock(3)
+//node3.handleRequest(1, 2)
+nodes = [node1, node2, node3]
+
+request(1,1)
+
+function request(senderId, senderTs) {
+    nodes.forEach((n) => {
+        if (n.id != senderId) {
+            n.handleRequest(senderId, senderTs)
+        }
+    })
+}
 
 //
--- app/dme-node.js
+++ app/dme-node.js
@@ -1,68 +1,101 @@
 const log = require('../log');
-module.exports = {
-    id: 0,
-    nodeCount: 0,
-    rd_array: [],
-    ts: 0,
-    received:[],
-    sent:[],
-
-    init: function (nodeId, totalCount) {
+class Dnode {
+    constructor(nodeId, totalCount) {
         this.id = nodeId;
-        this.nodeCount=totalCount;
+        this.nodeCount = totalCount;
+//        this.listeners=listeners;
         this.ts = 0;
-        this.rd_array=[]
-        this.received=[]
-        this.sent=[]
+        this.rd_array = []
         while (totalCount > 0) {
             this.rd_array.push(0)
-            this.received.push([])
-            this.sent.push([])
+            // this.received.push([])
+            // this.sent.push([])
             totalCount--;
         }
-        log("node initiated")
-    },
-
-    request_cs: function () {
-        // - requesting
-        // - When a site Si wants to enter the CS, it broadcasts a timestamped REQUEST message to all other sites
-        this.ts++
-        log(this.ts)
-    },
-
-    handle_request: function (sender_id, sender_ts) {
-        // - When site Sj receives a REQUEST message from site Si, 
-        // it sends a REPLY message to site Si if site Sj is neither requesting nor executing the CS, 
-        // or if the site Sj is requesting and Si’s request’s timestamp is smaller than site Sj’s own request’s timestamp. 
-        // Otherwise, the reply is deferred and Sj sets RDj [i] = 1
-        this.send_reply(sender_id)
-        this.ts++
-        log(this.ts)
-    },
-
-    send_reply: function(send_to_id){
-        
-        this.ts++
-        log(this.ts)
-    },
-
-    execute_cs: function () {
-        log(`node ${id} executing cs`)
-        i = 10
-        while (i > 0) {
-            i--
-            this.ts++
-        log(this.ts)
-        }
-    },
-
-    release_cs: function () {
-        // - When site Si exits the CS, it sends all the deferred REPLY messages: 
-        // ∀j if RDi [j] = 1, then Si  sends a REPLY message to Sj and sets RDi [j] = 0
-        this.ts++
+        log(`node ${this.id} created`)
+    }
+    advanceClock(refTs) {
+        this.ts = refTs > this.ts ? refTs + 1 : this.ts + 1
         log(this.ts)
     }
 
-    // request_cs()
-    // handle_request()
+    handleRequest(senderId, senderTs) {
+        
+        log(`in node ${this.id}`)
+        log(`handleRequest ${senderId} to ${this.id}`)
+    }
+
+    handleReply(senderId, senderTs) {
+        log('handleReply')
+
+        log(senderId)
+        log(senderTs)
+
+    }
 }
+module.exports = Dnode
+// module.exports = {
+//     id: 0,
+//     nodeCount: 0,
+//     rd_array: [],
+//     ts: 0,
+//     listeners:[],
+
+//     init: function (nodeId, totalCount) {
+//         this.id = nodeId;
+//         this.nodeCount=totalCount;
+//         this.ts = 0;
+//         this.rd_array=[]
+//         while (totalCount > 0) {
+//             this.rd_array.push(0)
+//             this.received.push([])
+//             this.sent.push([])
+//             totalCount--;
+//         }
+//         log(`node ${this.id} created`)
+//         return this.handle_request;
+//     },
+
+//     request_cs: function () {
+//         // - requesting
+//         // - When a site Si wants to enter the CS, it broadcasts a timestamped REQUEST message to all other sites
+//         this.ts++
+//         log(this.ts)
+//     },
+
+//     handle_request: function (sender_id, sender_ts) {
+//         // - When site Sj receives a REQUEST message from site Si, 
+//         // it sends a REPLY message to site Si if site Sj is neither requesting nor executing the CS, 
+//         // or if the site Sj is requesting and Si’s request’s timestamp is smaller than site Sj’s own request’s timestamp. 
+//         // Otherwise, the reply is deferred and Sj sets RDj [i] = 1
+//         this.send_reply(sender_id)
+//         this.ts++
+//         log(this.ts)
+//     },
+
+//     send_reply: function(send_to_id){
+
+//         this.ts++
+//         log(this.ts)
+//     },
+
+//     execute_cs: function () {
+//         log(`node ${id} executing cs`)
+//         i = 10
+//         while (i > 0) {
+//             i--
+//             this.ts++
+//         log(this.ts)
+//         }
+//     },
+
+//     release_cs: function () {
+//         // - When site Si exits the CS, it sends all the deferred REPLY messages: 
+//         // ∀j if RDi [j] = 1, then Si  sends a REPLY message to Sj and sets RDi [j] = 0
+//         this.ts++
+//         log(this.ts)
+//     }
+
+//     // request_cs()
+//     // handle_request()
+// }
